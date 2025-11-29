e393a8245d4f6cf829a3255db2de83343e999bd1
K S Noor Ahamad <nkurnipalli34@gmail.com>
2025-11-29 10:38:56 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score, mean_squared_error
+import pickle
 
 # Load dataset
 df = pd.read_csv("../StudentsPerformance.csv")
@@ -25,3 +26,9 @@ predictions = model.predict(X_test)
 # Evaluation
 print("R2 Score:", r2_score(y_test, predictions))
 print("MSE:", mean_squared_error(y_test, predictions))
+
+# Save model
+with open("model.pkl", "wb") as file:
+    pickle.dump(model, file)
+
+print("Model saved as model.pkl")
